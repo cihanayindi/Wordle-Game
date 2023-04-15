f19f97395624536d7aa4f716e315e9cf039578b9
Cihan Ayindi <cihan_ayindi@hotmail.com>
2023-04-15 16:08:35 +0300

    Javascript fonksiyonu güncellendi
    iftardan sonra devam

--- index.js
+++ index.js
@@ -2,22 +2,37 @@
 // oluşturdum ordan okutma işlemi daha iyi olur ona bakacağım
 
 let kelimeler = ["RADYO", "SEHPA", "DOLAP", "KALEM", "KAVUN", "KÖPEK", "MERAK", "GAZAP", "ROMAN", "CEVİZ"]
-
+var kelime = kelimeler[Math.floor(Math.random() * kelimeler.length)];
 // html kodunda yapılan kelime girişini yakalıyor
+
+let sayac = 1;
 
 const guessInput = document.querySelector('.guess-control');
 
 // butona tıklanınca çalışacak fonksiyon
 document.querySelector('#check-button').addEventListener('click', function() {
-    
     // boşluğa girilen string değerini büyük harflere dönüştürüp
     // kelimeler listesinin içinde var mı diye sorguluyor
     // eğer varsa direkt oyunu bitirecek yoksa hangi harfler 
     // yerler vb uyuşuyor diye bir geri dönüş yapacak.
-
-    if (kelimeler.includes(guessInput.value.toUpperCase())) {
-        console.log("evet içeride")
-    } else {
-        console.log("hayır içerde değil")
+    if (sayac < 7) {
+        if (inputValue == kelime) {
+            console.log("doğru tahmin");
+        } else {
+            for (var i = 0; i < inputValue.length; i++) {
+                var letter = inputValue[i];
+                if (kelime.includes(letter)) {
+                    var index = kelime.indexOf(letter);
+                    if (index == i) {
+                        console.log(letter, " harfi kelime içinde var ve yeri doğru. Kelime: ", kelime);
+                    } else {
+                        console.log(letter, " harfi kelime içinde var ama yeri doğru değil. Kelime: ", kelime); 
+                    }
+                } else {
+                    console.log(letter, " harfi kelimede yok. Kelime: ", kelime);
+                }
+            }
+        }
+        sayac++;
     }
 });
